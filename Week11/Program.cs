afff7bc1f707f5abe3a42d82af80055f97bd47ce
2기 최종혁 <jonghyuk.peter.choi@gmail.com>
2014-05-27 02:44:08 +0900

    과제 제출

---
+++ Week11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter bw = new BinaryWriter(new FileStream("a.dat", FileMode.Create));
+            bw.Write("조영호");
+            bw.Write("국어");
+            bw.Write(80);
+
+            bw.Close();
+
+            BinaryReader br = new BinaryReader(new FileStream("a.dat", FileMode.Open));
+
+            string name = br.ReadString();
+            string subject = br.ReadString();
+            int score = br.ReadInt32();
+
+            Console.WriteLine("{0}\t{1}\t{2}", name, subject, score);
+
+            br.Close();
+        }
+    }
+}
